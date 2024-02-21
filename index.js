70013f2c927b8e6f9f7790f10aac6d38b6b6e608
Dalton Riehl <dtonriehl@gmail.com>
2024-02-21 17:13:30 -0600

    Functions Lab Complete: Achievement Get!

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Code your solution in this file!
+const homeStreet = 42;
+const distanceFromHqInBlocks = (passengerLocation) => {
+    let numberOfBlocks = 0;
+    if (passengerLocation > homeStreet) {
+        numberOfBlocks = passengerLocation - homeStreet;
+    }
+    else if (homeStreet > passengerLocation) {
+        numberOfBlocks = homeStreet - passengerLocation;
+    }
+    return numberOfBlocks;
+}
+
+const distanceFromHqInFeet = (passengerLocation) => {
+    let distance = distanceFromHqInBlocks(passengerLocation);
+    let distanceInFeet = distance * 264;
+    return distanceInFeet;
+}
+
+const distanceTravelledInFeet = (startBlock, endBlock) => {
+let blocks = Math.abs(startBlock - endBlock);
+let blocksInFeet = blocks * 264;
+return blocksInFeet;
+}
+
+const calculatesFarePrice = (startBlock, endBlock) => {
+    let fare = 0.00;
+    let distance = distanceTravelledInFeet(startBlock, endBlock);
+    if (distance <= 400) {
+        return fare;
+    }
+    else if (distance > 400 && distance <= 2000) {
+        fare = 0.02 * (distance - 400);
+        return fare;
+    }
+    else if (distance > 2000 && distance <= 2500) {
+        fare = 25;
+        return fare;
+    }
+    else if (distance > 2500) {
+        return 'cannot travel that far';
+    }
+}
